601aff440b48be69e9adcb18453465935a01243d
Patricio Vargas <pevargas2730@gmail.com>
2023-06-05 20:05:36 -0400

    backend code working

--- public/app.js
+++ public/app.js
@@ -1,41 +1,76 @@
-paypal
-  .Buttons({
-    // Sets up the transaction when a payment button is clicked
-    createOrder: function (data, actions) {
-      return fetch("/api/orders", {
-        method: "post",
-        // use the "body" param to optionally pass additional order information
-        // like product ids or amount
-      })
-        .then((response) => response.json())
-        .then((order) => order.id);
-    },
-    // Finalize the transaction after payer approval
-    onApprove: function (data, actions) {
-      return fetch(`/api/orders/${data.orderID}/capture`, {
-        method: "post",
-      })
-        .then((response) => response.json())
-        .then((orderData) => {
-          // Successful capture! For dev/demo purposes:
-          console.log(
-            "Capture result",
-            orderData,
-            JSON.stringify(orderData, null, 2)
-          );
-          const transaction = orderData.purchase_units[0].payments.captures[0];
-          alert(`Transaction ${transaction.status}: ${transaction.id}
+const FUNDING_SOURCES = [
+  paypal.FUNDING.PAYPAL,
+  paypal.FUNDING.PAYLATER,
+  paypal.FUNDING.VENMO,
+];
 
-            See console for all available details
-          `);
-          // When ready to go live, remove the alert and show a success message within this page. For example:
-          // var element = document.getElementById('paypal-button-container');
-          // element.innerHTML = '<h3>Thank you for your payment!</h3>';
-          // Or go to another URL:  actions.redirect('thank_you.html');
+FUNDING_SOURCES.forEach((fundingSource) => {
+  paypal
+    .Buttons({
+      fundingSource,
+      style: {
+        layout: 'vertical',
+        shape: 'rect',
+        color: fundingSource === paypal.FUNDING.PAYLATER ? 'gold' : '',
+      },
+    createOrder: async (data, actions) => {
+      try {
+        const response = await fetch('/api/orders', {
+          method: 'POST',
         });
+
+        const details = await response.json();
+        return details.id;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onApprove: async (data, actions) => {
+      try {
+        const response = await fetch(`/api/orders/${data.orderID}/capture`,
+          {
+            method: 'POST',
+          }
+        );
+
+        const details = await response.json();
+        // Three cases to handle:
+        //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+        //   (2) Other non-recoverable errors -> Show a failure message
+        //   (3) Successful transaction -> Show confirmation or thank you message
+
+        // This example reads a v2/checkout/orders capture response, propagated from the server
+        // You could use a different API or structure for your 'orderData'
+        const errorDetail =
+          Array.isArray(details.details) && details.details[0];
+
+        if (
+          errorDetail &&
+          errorDetail.issue === 'INSTRUMENT_DECLINED'
+        ) {
+          return actions.restart();
+          // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
+        }
+
+        if (errorDetail) {
+          let msg = 'Sorry, your transaction could not be processed.';
+          msg += errorDetail.description
+            ? ' ' + errorDetail.description
+            : '';
+          msg += details.debug_id ? ' (' + details.debug_id + ')' : '';
+          alert(msg);
+        }
+
+        const transaction =
+          details.purchase_units[0].payments.captures[0];
+        alert('Transaction ' + transaction.status + ': ' + transaction.id + 'See console for all available details');
+      } catch (error) {
+        console.error(error);
+      }
     },
   })
-  .render("#paypal-button-container");
+  .render("#paypal-button-container")
+});
 
 // If this returns false or the card fields aren't visible, see Step #1.
 if (paypal.HostedFields.isEligible()) {
@@ -44,17 +79,18 @@ if (paypal.HostedFields.isEligible()) {
   // Renders card fields
   paypal.HostedFields.render({
     // Call your server to set up the transaction
-    createOrder: () => {
-      return fetch("/api/orders", {
-        method: "post",
-        // use the "body" param to optionally pass additional order information like
-        // product ids or amount.
-      })
-        .then((res) => res.json())
-        .then((orderData) => {
-          orderId = orderData.id; // needed later to complete capture
-          return orderData.id;
+    createOrder: async(data, actions) => {
+      try {
+        const response = await fetch('/api/orders', {
+          method: 'POST',
         });
+
+        const details = await response.json();
+        orderId = details.id;
+        return orderId;
+      } catch (error) {
+        console.error(error);
+      }
     },
     styles: {
       ".valid": {
@@ -87,23 +123,8 @@ if (paypal.HostedFields.isEligible()) {
           cardholderName: document.getElementById("card-holder-name").value,
           // Billing Address
           billingAddress: {
-            // Street address, line 1
-            streetAddress: document.getElementById(
-              "card-billing-address-street"
-            ).value,
-            // Street address, line 2 (Ex: Unit, Apartment, etc.)
-            extendedAddress: document.getElementById(
-              "card-billing-address-unit"
-            ).value,
-            // State
-            region: document.getElementById("card-billing-address-state").value,
-            // City
-            locality: document.getElementById("card-billing-address-city")
-              .value,
-            // Postal Code
             postalCode: document.getElementById("card-billing-address-zip")
               .value,
-            // Country Code
             countryCodeAlpha2: document.getElementById(
               "card-billing-address-country"
             ).value,
